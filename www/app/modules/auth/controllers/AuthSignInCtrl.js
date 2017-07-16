fb301f0142c37ea4a325b9e66f78bd9efca311a6
mutantsint <sudeshnt93@gmail.com>
2017-07-16 15:04:23 +0530

    item list UI to home page

--- www/app/modules/auth/controllers/AuthSignInCtrl.js
+++ www/app/modules/auth/controllers/AuthSignInCtrl.js
@@ -5,7 +5,7 @@ angular.module('auth.module').controller('AuthSignInCtrl', AuthSignInCtrl);
 AuthSignInCtrl.$inject = ['$scope','$state','$rootScope'];
 
 function AuthSignInCtrl($scope,$state,$rootScope) {
-  console.log('auth sign in');
+  //console.log('auth sign in');
 
   $scope.signIn = function () {
     $state.go('home.new');

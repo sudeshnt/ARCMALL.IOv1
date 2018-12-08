812db762d3661051062f3836423a1d6922195106
Rajitha Perera <rajitha@alliontechnologies.com>
2018-12-08 18:05:35 +0530

    add language

--- www/app/modules/settings/controllers/LanguagesCtrl.js
+++ www/app/modules/settings/controllers/LanguagesCtrl.js
@@ -1,34 +1,55 @@
+"use strict";
 
-'use strict';
+angular.module("settings.module").controller("LanguagesCtrl", LanguagesCtrl);
 
-angular.module('settings.module').controller('LanguagesCtrl', LanguagesCtrl);
+CartCtrl.$inject = [
+  "$scope",
+  "$state",
+  "$rootScope",
+  "$timeout",
+  "$mdBottomSheet",
+  "$mdToast",
+  "cartSev",
+  "serverConfig",
+  "httpService",
+  "$httpParamSerializer",
+  "$ionicHistory"
+];
 
-CartCtrl.$inject = ['$scope','$state','$rootScope','$timeout', '$mdBottomSheet',
-'$mdToast','cartSev','serverConfig','httpService','$httpParamSerializer', '$ionicHistory'];
+function LanguagesCtrl(
+  $scope,
+  $state,
+  $rootScope,
+  $timeout,
+  $mdBottomSheet,
+  $mdToast,
+  cartSev,
+  serverConfig,
+  httpService,
+  $httpParamSerializer,
+  $ionicHistory
+) {
+  var lang = localStorage.getItem("language");
+  if (!lang || lang == "" || lang === "undefined" || lang == null) {
+    lang = "en";
+  }
 
-function LanguagesCtrl($scope,$state,$rootScope, $timeout, $mdBottomSheet,
-  $mdToast,cartSev,serverConfig,httpService,$httpParamSerializer, $ionicHistory) {
+  $scope.choice = lang;
 
-    var lang = localStorage.getItem('language');
-    if(!lang || lang == '' || lang === 'undefined' || lang == null){
-      lang = 'en';
-    }
+  $scope.languages = [
+    { name: "English (US)", code: "en" },
+    { name: "Simplified Chinese (简体中文)", code: "zh" }
+  ];
 
-    $scope.choice = lang;
+  function isCurrentLang(language) {
+    return language == lang;
+  }
 
-    $scope.languages = [
-      { name: 'English (US)', code: 'en'},
-      { name: 'Simplified Chinese (简体中文)', code:'zh'},
-    ];
-
-    function isCurrentLang(language) {
-      return (language == lang)
-    }
-
-    $scope.changeLanguage = function(lang){
-      localStorage.removeItem('cat_tabs');
-      localStorage.setItem('language', lang);
-      localStorage.setItem('language_changed', true);      
-      location.reload();
-    }
+  $scope.changeLanguage = function(lang) {
+    console.log("changed");
+    localStorage.removeItem("cat_tabs");
+    localStorage.setItem("language", lang);
+    localStorage.setItem("language_changed", true);
+    location.reload();
+  };
 }
